b84a5e4a39843289e8a4ce6f34ba962ee3c0735d
Nick <grimchains@gmail.com>
2013-04-02 16:41:14 -0400

    Managed to get tabbed panes working.

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,45 @@
+import javax.swing.JTabbedPane;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.*;
 
-public class GUI {
+public class GUI extends JPanel {
 
+	public GUI() {
+		JTabbedPane jtbExample = new JTabbedPane();
+		JPanel jplInnerPanel1 = createInnerPanel("Tab 1 Contains Tooltip and Icon");
+		jtbExample.addTab("Sensor Reading", jplInnerPanel1);
+		jtbExample.setSelectedIndex(0);
+		JPanel jplInnerPanel2 = createInnerPanel("Tab 2 contains icon only");
+		jtbExample.addTab("Source Code", jplInnerPanel2);
+		setLayout(new GridLayout(1, 1));
+		add(jtbExample);
+	}
+
+	protected JPanel createInnerPanel(String text) {
+		JPanel jplPanel = new JPanel();
+		JLabel jlbDisplay = new JLabel(text);
+		jlbDisplay.setHorizontalAlignment(JLabel.CENTER);
+		jplPanel.setLayout(new GridLayout(1, 1));
+		jplPanel.add(jlbDisplay);
+		return jplPanel;
+	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Debug GUI");
+		frame.addWindowListener(new WindowAdapter() {
+
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		frame.getContentPane().add(new GUI(),
+				BorderLayout.CENTER);
+		frame.setSize(400, 125);
+		frame.setVisible(true);
+	}
+	
 }
