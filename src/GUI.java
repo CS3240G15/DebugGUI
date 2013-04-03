14e51a0f3e6e3aab460ca85414d0227d4fc4c19c
Nick <grimchains@gmail.com>
2013-04-02 23:44:54 -0400

    Moved GUI into GUI_Test, and made GUI. GUI will be the actual prototype/product.

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,4 @@
+import javax.swing.JButton;
 import javax.swing.JTabbedPane;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -6,40 +7,64 @@ import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.*;
 
-public class GUI extends JPanel {
-
-	public GUI() {
-		JTabbedPane jtbExample = new JTabbedPane();
-		JPanel jplInnerPanel1 = createInnerPanel("Tab 1 Contains Tooltip and Icon");
-		jtbExample.addTab("Sensor Reading", jplInnerPanel1);
-		jtbExample.setSelectedIndex(0);
-		JPanel jplInnerPanel2 = createInnerPanel("Tab 2 contains icon only");
-		jtbExample.addTab("Source Code", jplInnerPanel2);
-		setLayout(new GridLayout(1, 1));
-		add(jtbExample);
-	}
-
-	protected JPanel createInnerPanel(String text) {
-		JPanel jplPanel = new JPanel();
-		JLabel jlbDisplay = new JLabel(text);
-		jlbDisplay.setHorizontalAlignment(JLabel.CENTER);
-		jplPanel.setLayout(new GridLayout(1, 1));
-		jplPanel.add(jlbDisplay);
-		return jplPanel;
-	}
+public class GUI extends JPanel{
 	
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Debug GUI");
+	private JFrame frame;
+	private JPanel debugPanel, sensorPanel;
+	
+	private void GUI() {
+		frame = new JFrame("Debug GUI");
 		frame.addWindowListener(new WindowAdapter() {
 
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
 		});
-		frame.getContentPane().add(new GUI(),
-				BorderLayout.CENTER);
-		frame.setSize(400, 125);
+		frame.getContentPane().setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		
+		c.fill = GridBagConstraints.BOTH;
+		c.weightx = 0.5;
+		c.weighty = 0.5;
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridheight = 2;
+		frame.getContentPane().add(makeTabSection(), c);
+		
+		frame.setSize(600, 800);
 		frame.setVisible(true);
 	}
 	
+	private JTabbedPane makeTabSection() {
+		JTabbedPane pane = new JTabbedPane();
+		JPanel panel =  createSensorPanel();
+		pane.addTab("Sensor", panel);
+		panel = createDebugPanel();
+		pane.addTab("Debug", panel);
+		return pane;
+	}
+	
+	private JPanel createSensorPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 0));
+		JLabel sensorInfo = new JLabel("Laura Ipsen");
+		panel.add(sensorInfo);
+		JLabel uplinkInfo = new JLabel("Uplink info");
+		panel.add(uplinkInfo);
+		return panel;
+	}
+	
+	private JPanel createDebugPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 0));
+		JLabel sourceCode = new JLabel("Source Code Placeholder text");
+		panel.add(sourceCode);
+		JLabel debuggingInfo = new JLabel("Single Steps, Breakpoints, etc.");
+		panel.add(debuggingInfo);
+		return panel;
+	}
+	
+	public static void main(String[] args) {
+		GUI gui = new GUI(); // A somewhat awkward way of getting around having to static EVERYTHING.
+	}
 }
