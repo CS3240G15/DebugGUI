f31aa9f391b9aabbe809e232284cf9f81aa92eaf
Nick <grimchains@gmail.com>
2013-04-22 16:22:36 -0400

    Removed some unused imports

--- src/Communicator.java
+++ src/Communicator.java
@@ -2,12 +2,10 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import lejos.pc.comm.NXTComm;
 import lejos.pc.comm.NXTCommFactory;
